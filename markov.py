0493a34b413049eca60798cc12d944b627412c43
Hackbright Student <user@mini-007.(none)>
2014-02-12 10:43:22 -0800

    build the string

--- markov.py
+++ markov.py
@@ -3,26 +3,11 @@
 import sys
 import random
 
-
-
-#------------------------------
-#  The story, so far.. 
-# 
-# 1. read the file (done)
-# 2. copy the file into a single string/variable )done)
-# 3. close the file (done)
-# 4. split the string into words (done)
-# 5. identify the number of words in the new list (done)
-# 6. crawl through the list and pair:
-#     word 1 + word 2
-#        if this doesn't exist as a tuple already, add it, with the third word as a value
-#        if this does exist as a tuple already, append the next word to the value list
-
-#def make_chains(corpus):
-
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
-    markov chains."""
+markov chains."""
+
+    # create our empty dictionary
     dictionary = {}
 
     split_words = corpus.split()
@@ -34,54 +19,84 @@ def make_chains(corpus):
         else:
             dictionary[(split_words[i], split_words[i+1])] = [split_words[i+2]]
 
-    print dictionary
+#    print dictionary
     return dictionary
 
 
-# ---------- this is all intact from the sample 
+# ---------- this is all intact from the sample
 #
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
-    based off an original text."""
+based off an original text."""
     keylist = chains.keys()
     first_tuple = random.choice(keylist)
+    # could try the below by accessing first_tuple[0] and first_tuple[1]
 
-#    for i in range(10):
- #       print first_tuple
-
-# for i in xrange(maxwords):
-#     newword = random.choice(table[(w1, w2)])
-#     if newword == nonword: sys.exit()
-#     print newword;
-#     w1, w2 = w2, newword
+    # create variables "word1" and "word2" and set them to the two words from the tuple
+    word1, word2 = first_tuple # might need parentheses
     
-#     for (word1, word2) in dictionary
+    # we can also reference the first and second words by their index numbers in the tuple:
+#    print first_tuple[0]
+#    print first_tuple[1]
 
+    # build the list: the first two items are the first tuple
 
-    # word1, word2 = word2, next_word
+    # create an "output_list" that starts with the first two words from above
 
-    # print word1
-    # print word2
-    # print next_word
+    output_list = [word1, word2]
+#    print output_list
 
+    # here is our loop 
 
-#    return "Here's some random text."
+    for i in range(20):   # <- set max of 20 so that the list doesn't get too long
+
+        # get our "next word" as a random choice from the value list from our tuple
+        # (the tuple is written here as "chains[(word1, word2)]")
+        next_word = random.choice(chains[(word1, word2)])
+
+        # copy word2 over to word1, and next_word over to word2, to form our next tuple
+        word1, word2 = word2, next_word
+
+        # print word1
+        # print word2
+        # print next_word
+
+        # add next_word to the end of our output_list
+        output_list.append(next_word)
+
+        # print output_list
+
+    # once we have our list, we can create the string by combining the list
+    # and separating each word with a space
+    output_string = " ".join(output_list)
+    
+    # this changes the first character of our string (output_string[0])
+    # to an upper case. 
+#    output_string = output_string[0].upper() + output_string[1:]
+    output_string_punctuated = output_string + "."
+
+#    print output_string
+    return output_string_punctuated
+
+# join outside of for loop
+
+# return "Here's some random text."
 
 def main():
     args = sys.argv
 
     # Change this to read input_text from a file
     f = open('blender.txt', 'r')
-      #read the file into another copy 
+      #read the file into another copy
     input_text = f.read()
       #close original file
     f.close()
-
+    
+    input_text = input_text.lower()
+    
     chain_dict = make_chains(input_text)
     random_text = make_text(chain_dict)
     print random_text
 
 if __name__ == "__main__":
     main()
-
-
